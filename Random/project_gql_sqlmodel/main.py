37100e056990f32007bd91e13bf6e73e5ba0367b
maciej.dobrzanski <matt950806@wp.pl>
2024-10-07 08:56:53 +0200

    Add SSE example

--- Random/project_gql_sqlmodel/main.py
+++ Random/project_gql_sqlmodel/main.py
@@ -1,4 +1,9 @@
+import datetime
+from asyncio import sleep
+
 from fastapi import FastAPI
+from starlette.responses import StreamingResponse
+
 from app.api import router as api_router
 from app.graphql import graphql_app
 
@@ -9,3 +14,13 @@ app.include_router(api_router, prefix="/api")
 
 # Add GraphQL endpoint using Graphene's ASGIApp
 app.add_route("/graphql", graphql_app)
+
+
+async def example_logs_generator():
+    while True:
+        yield f"{datetime.datetime.utcnow()}: Example log"
+        await sleep(1)
+
+@app.get("/get-logs")
+async def root():
+    return StreamingResponse(example_logs_generator(), media_type="text/event-stream")
